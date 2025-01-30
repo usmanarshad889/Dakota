110c687390a40ce29339db75475e688ac3f61a55
usmaan.arshad <usman.arshad@rolustech.com>
2025-01-30 18:52:02 +0500

    added new test

---
+++ tests/test_mass_create_link_account_by_crd.py
@@ -0,0 +1,76 @@
+import time
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+@pytest.fixture(scope="module")
+def driver():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
+
+# Fixture to load the configuration
+@pytest.fixture(scope="module")
+def config(request):
+    import json
+    import os
+    env = request.config.getoption("--env")
+    config_file_path = os.path.join("config", f"config.{env}.json")
+    with open(config_file_path) as file:
+        return json.load(file)
+
+def test_mass_create_link_account_by_crd(driver, config):
+    # Navigate to login page
+    driver.get(config["base_url"])
+    wait = WebDriverWait(driver, 10)
+
+    # Perform login
+    username = wait.until(EC.element_to_be_clickable((By.ID, "username")))
+    username.send_keys(config["username"])
+
+    password = wait.until(EC.element_to_be_clickable((By.ID, "password")))
+    password.send_keys(config["password"])
+
+    login_button = wait.until(EC.element_to_be_clickable((By.ID, "Login")))
+    login_button.click()
+    time.sleep(3)
+
+    # Navigate to installed pakages setup
+    driver.get(f"{config["base_url"]}lightning/n/Marketplace__Dakota_Search")
+    time.sleep(13)
+
+    dropdown = driver.find_element(By.XPATH, "//select[@name='DisplayCriteria']")
+    dropdown_option = Select(dropdown)
+    dropdown_option.select_by_visible_text("Unlinked Accounts")
+    time.sleep(1)
+
+    driver.find_element(By.XPATH, "//button[@title='Search']").click()
+    time.sleep(10)
+
+    # Click on ALL CHECKBOX
+    driver.find_element(By.XPATH, "(//span[@class='slds-checkbox_faux'])[1]").click()
+    time.sleep(1)
+
+    # Click on linked account
+    dropdown = driver.find_element(By.XPATH, "//select[@name='MassUploadActions']")
+    dropdown_option = Select(dropdown)
+    dropdown_option.select_by_visible_text("Link Account")
+    time.sleep(10)
+
+    # Click on linked account
+    dropdown = driver.find_element(By.XPATH, "(//select[@class='slds-select'])[4]")
+    dropdown_option = Select(dropdown)
+    dropdown_option.select_by_visible_text("Dakota CRD#")
+    driver.find_element(By.XPATH, "//button[contains(@class,'slds-button slds-button_neutral slds-button slds-button--brand')][normalize-space()='Search']").click()
+    time.sleep(10)
+
+
+    # Click on linked account
+    driver.find_element(By.XPATH, "//button[normalize-space()='Link Accounts']").click()
+    time.sleep(10)
+    driver.quit()
