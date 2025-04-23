68b32250db33382f14335615515c8da47d773d2a
usmaan.arshad <usman.arshad@rolustech.com>
2025-04-23 10:27:29 +0500

    Added testcases

--- tests/Critical_Scenarios_for_Smoke_Testing/test_contact_record_auto_sync.py
+++ tests/Critical_Scenarios_for_Smoke_Testing/test_contact_record_auto_sync.py
@@ -14,7 +14,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-value_src = "uuu"
+value_src = "vvv"
 
 
 # Initialize Faker
